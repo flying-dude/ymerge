5252dfb1a28a6b29c8e3b86c4ff14946b7ec6238
flying-dude <dude@flyspace.dev>
2022-07-07 22:50:27 +0200

    reformat: pacman.cc pacman.hh

--- src/auracle/auracle/pacman.cc
+++ src/auracle/auracle/pacman.cc
@@ -16,42 +16,35 @@ namespace {
 class Glob {
  public:
   explicit Glob(std::string_view pattern) {
-    glob_ok_ =
-        glob(std::string(pattern).c_str(), GLOB_NOCHECK, nullptr, &glob_) == 0;
-    if (glob_ok_) {
-      results_ = absl::MakeSpan(glob_.gl_pathv, glob_.gl_pathc);
-    }
+    glob_ok_ = glob(std::string(pattern).c_str(), GLOB_NOCHECK, nullptr, &glob_) == 0;
+    if (glob_ok_) { results_ = absl::MakeSpan(glob_.gl_pathv, glob_.gl_pathc); }
   }
 
   bool ok() const { return glob_ok_; }
 
   ~Glob() {
-    if (glob_ok_) {
-      globfree(&glob_);
-    }
+    if (glob_ok_) { globfree(&glob_); }
   }
 
-  using iterator = absl::Span<char*>::iterator;
+  using iterator = absl::Span<char *>::iterator;
   iterator begin() { return results_.begin(); }
   iterator end() { return results_.end(); }
 
  private:
   glob_t glob_;
   bool glob_ok_;
-  absl::Span<char*> results_;
+  absl::Span<char *> results_;
 };
 
 class FileReader {
  public:
-  explicit FileReader(const std::string& path) : file_(path) {}
+  explicit FileReader(const std::string &path) : file_(path) {}
 
   ~FileReader() {
-    if (ok()) {
-      file_.close();
-    }
+    if (ok()) { file_.close(); }
   }
 
-  bool GetLine(std::string& line) { return bool(std::getline(file_, line)); }
+  bool GetLine(std::string &line) { return bool(std::getline(file_, line)); }
 
   bool ok() const { return file_.is_open(); }
 
@@ -59,16 +52,11 @@ class FileReader {
   std::ifstream file_;
 };
 
-bool IsSection(std::string_view s) {
-  return s.size() > 2 && s.front() == '[' && s.back() == ']';
-}
+bool IsSection(std::string_view s) { return s.size() > 2 && s.front() == '[' && s.back() == ']'; }
 
-std::pair<std::string_view, std::string_view> SplitKeyValue(
-    std::string_view line) {
+std::pair<std::string_view, std::string_view> SplitKeyValue(std::string_view line) {
   auto equals = line.find('=');
-  if (equals == line.npos) {
-    return {line, ""};
-  }
+  if (equals == line.npos) { return {line, ""}; }
 
   return {absl::StripTrailingAsciiWhitespace(line.substr(0, equals)),
           absl::StripLeadingAsciiWhitespace(line.substr(equals + 1))};
@@ -78,8 +66,7 @@ std::pair<std::string_view, std::string_view> SplitKeyValue(
 
 namespace auracle {
 
-Pacman::Pacman(alpm_handle_t* alpm)
-    : alpm_(alpm), local_db_(alpm_get_localdb(alpm_)) {}
+Pacman::Pacman(alpm_handle_t *alpm) : alpm_(alpm), local_db_(alpm_get_localdb(alpm_)) {}
 
 Pacman::~Pacman() { alpm_release(alpm_); }
 
@@ -91,14 +78,12 @@ struct ParseState {
   std::vector<std::string> repos;
 };
 
-bool ParseOneFile(const std::string& path, ParseState* state) {
+bool ParseOneFile(const std::string &path, ParseState *state) {
   FileReader reader(path);
 
   for (std::string buffer; reader.GetLine(buffer);) {
     std::string_view line = absl::StripAsciiWhitespace(buffer);
-    if (line.empty() || line[0] == '#') {
-      continue;
-    }
+    if (line.empty() || line[0] == '#') { continue; }
 
     if (IsSection(line)) {
       state->section = line.substr(1, line.size() - 2);
@@ -123,14 +108,10 @@ bool ParseOneFile(const std::string& path, ParseState* state) {
 
     if (key == "Include") {
       Glob includes(value);
-      if (!includes.ok()) {
-        return false;
-      }
+      if (!includes.ok()) { return false; }
 
-      for (const auto* p : includes) {
-        if (!ParseOneFile(p, state)) {
-          return false;
-        }
+      for (const auto *p : includes) {
+        if (!ParseOneFile(p, state)) { return false; }
       }
     }
   }
@@ -139,50 +120,39 @@ bool ParseOneFile(const std::string& path, ParseState* state) {
 }
 
 // static
-std::unique_ptr<Pacman> Pacman::NewFromConfig(const std::string& config_file) {
+std::unique_ptr<Pacman> Pacman::NewFromConfig(const std::string &config_file) {
   ParseState state;
 
-  if (!ParseOneFile(config_file, &state)) {
-    return nullptr;
-  }
+  if (!ParseOneFile(config_file, &state)) { return nullptr; }
 
   alpm_errno_t err;
-  alpm_handle_t* alpm = alpm_initialize("/", state.dbpath.c_str(), &err);
-  if (alpm == nullptr) {
-    return nullptr;
-  }
+  alpm_handle_t *alpm = alpm_initialize("/", state.dbpath.c_str(), &err);
+  if (alpm == nullptr) { return nullptr; }
 
-  for (const auto& repo : state.repos) {
-    alpm_register_syncdb(alpm, repo.c_str(), alpm_siglevel_t(0));
-  }
+  for (const auto &repo : state.repos) { alpm_register_syncdb(alpm, repo.c_str(), alpm_siglevel_t(0)); }
 
   return std::unique_ptr<Pacman>(new Pacman(alpm));
 }
 
-std::string Pacman::RepoForPackage(const std::string& package) const {
+std::string Pacman::RepoForPackage(const std::string &package) const {
   for (auto i = alpm_get_syncdbs(alpm_); i != nullptr; i = i->next) {
-    auto db = static_cast<alpm_db_t*>(i->data);
+    auto db = static_cast<alpm_db_t *>(i->data);
     auto pkgcache = alpm_db_get_pkgcache(db);
 
-    if (alpm_find_satisfier(pkgcache, package.c_str()) != nullptr) {
-      return alpm_db_get_name(db);
-    }
+    if (alpm_find_satisfier(pkgcache, package.c_str()) != nullptr) { return alpm_db_get_name(db); }
   }
 
   return std::string();
 }
 
-bool Pacman::DependencyIsSatisfied(const std::string& package) const {
-  auto* cache = alpm_db_get_pkgcache(local_db_);
+bool Pacman::DependencyIsSatisfied(const std::string &package) const {
+  auto *cache = alpm_db_get_pkgcache(local_db_);
   return alpm_find_satisfier(cache, package.c_str()) != nullptr;
 }
 
-std::optional<Pacman::Package> Pacman::GetLocalPackage(
-    const std::string& name) const {
-  auto* pkg = alpm_db_get_pkg(local_db_, name.c_str());
-  if (pkg == nullptr) {
-    return std::nullopt;
-  }
+std::optional<Pacman::Package> Pacman::GetLocalPackage(const std::string &name) const {
+  auto *pkg = alpm_db_get_pkg(local_db_, name.c_str());
+  if (pkg == nullptr) { return std::nullopt; }
 
   return Package{alpm_pkg_get_name(pkg), alpm_pkg_get_version(pkg)};
 }
@@ -191,7 +161,7 @@ std::vector<Pacman::Package> Pacman::LocalPackages() const {
   std::vector<Package> packages;
 
   for (auto i = alpm_db_get_pkgcache(local_db_); i != nullptr; i = i->next) {
-    const auto pkg = static_cast<alpm_pkg_t*>(i->data);
+    const auto pkg = static_cast<alpm_pkg_t *>(i->data);
 
     packages.emplace_back(alpm_pkg_get_name(pkg), alpm_pkg_get_version(pkg));
   }
@@ -200,8 +170,6 @@ std::vector<Pacman::Package> Pacman::LocalPackages() const {
 }
 
 // static
-int Pacman::Vercmp(const std::string& a, const std::string& b) {
-  return alpm_pkg_vercmp(a.c_str(), b.c_str());
-}
+int Pacman::Vercmp(const std::string &a, const std::string &b) { return alpm_pkg_vercmp(a.c_str(), b.c_str()); }
 
 }  // namespace auracle
--- src/auracle/auracle/pacman.hh
+++ src/auracle/auracle/pacman.hh
@@ -13,43 +13,40 @@ namespace auracle {
 class Pacman {
  public:
   struct Package {
-    Package(std::string pkgname, std::string pkgver)
-        : pkgname(std::move(pkgname)), pkgver(std::move(pkgver)) {}
+    Package(std::string pkgname, std::string pkgver) : pkgname(std::move(pkgname)), pkgver(std::move(pkgver)) {}
     std::string pkgname;
     std::string pkgver;
   };
 
   // Factory constructor.
-  static std::unique_ptr<Pacman> NewFromConfig(const std::string& config_file);
+  static std::unique_ptr<Pacman> NewFromConfig(const std::string &config_file);
 
   ~Pacman();
 
-  Pacman(const Pacman&) = delete;
-  Pacman& operator=(const Pacman&) = delete;
+  Pacman(const Pacman &) = delete;
+  Pacman &operator=(const Pacman &) = delete;
 
-  Pacman(Pacman&&) = default;
-  Pacman& operator=(Pacman&&) = default;
+  Pacman(Pacman &&) = default;
+  Pacman &operator=(Pacman &&) = default;
 
-  static int Vercmp(const std::string& a, const std::string& b);
+  static int Vercmp(const std::string &a, const std::string &b);
 
   // Returns the name of the repo that the package belongs to, or empty string
   // if the package was not found in any repo.
-  std::string RepoForPackage(const std::string& package) const;
+  std::string RepoForPackage(const std::string &package) const;
 
-  bool HasPackage(const std::string& package) const {
-    return !RepoForPackage(package).empty();
-  }
+  bool HasPackage(const std::string &package) const { return !RepoForPackage(package).empty(); }
 
-  bool DependencyIsSatisfied(const std::string& package) const;
+  bool DependencyIsSatisfied(const std::string &package) const;
 
   std::vector<Package> LocalPackages() const;
-  std::optional<Package> GetLocalPackage(const std::string& name) const;
+  std::optional<Package> GetLocalPackage(const std::string &name) const;
 
  private:
-  Pacman(alpm_handle_t* alpm);
+  Pacman(alpm_handle_t *alpm);
 
-  alpm_handle_t* alpm_;
-  alpm_db_t* local_db_;
+  alpm_handle_t *alpm_;
+  alpm_db_t *local_db_;
 };
 
 }  // namespace auracle
