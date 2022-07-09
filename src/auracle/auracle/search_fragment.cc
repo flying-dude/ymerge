d79bed134a29d45b745f27614aee99fd81d8ae3f
flying-dude <dude@flyspace.dev>
2022-07-09 17:22:11 +0200

    avoid assertion for string_view, where it assumes you cannot go past string_view boundaries.
    now fixed but before could be reproduced with:
    $ ./build/auracle search anything
    /usr/include/c++/12.1.0/string_view:239: constexpr const std::basic_string_view<_CharT, _Traits>::value_type& std::basic_string_view<_CharT, _Traits>::operator[](size_type) const [with _CharT = char; _Traits = std::char_traits<char>; const_reference = const char&; size_type = long unsigned int]: Assertion '__pos < this->_M_len' failed.

--- src/auracle/auracle/search_fragment.cc
+++ src/auracle/auracle/search_fragment.cc
@@ -37,11 +37,31 @@ std::string_view GetSearchFragment(std::string_view input) {
       continue;
     }
 
+// clang-format off
+/* flydude 07/2022
+
+i got an assertion error at runtime from this trick, when compiling with "-Wp,-D_GLIBCXX_ASSERTIONS".
+
+$ ./build/auracle search anything
+/usr/include/c++/12.1.0/string_view:239: constexpr const std::basic_string_view<_CharT, _Traits>::value_type& std::basic_string_view<_CharT, _Traits>::operator[](size_type) const [with _CharT = char; _Traits = std::char_traits<char>; const_reference = const char&; size_type = long unsigned int]: Assertion '__pos < this->_M_len' failed.
+fish: Job 1, './build/auracle search anything' terminated by signal SIGABRT (Abort)
+
+these assertions can be disabled but the following cmake flag overrides it again: -DCMAKE_INSTALL_PREFIX:PATH='/usr'
+guessing that the C++ standard lib is compiled with assertions?
+==> solution: convert to c-string first and then obtain the character.
+
+*/
+// clang-format on
+
     // given 'cow?', we can't include w in the search
     // Looking one character past the end of the candidate is safe because we
     // know that our input is null delimited.
-    auto cand = input.substr(0, span);
-    if (cand[span] == '?' || cand[span] == '*') {
+    std::string_view cand = input.substr(0, span);
+    const char * cand_ = &cand[0];
+
+    // char c = cand[span]; // assertion error due to string_view overflow (see above)
+    char c = cand_[span];
+    if (c == '?' || c == '*') {
       cand.remove_suffix(1);
     }
 
