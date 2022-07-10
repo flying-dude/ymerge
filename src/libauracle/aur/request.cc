cfee46591f7d8a4d44b90f3045df4afee39b07f3
flying-dude <dude@flyspace.dev>
2022-07-10 14:51:38 +0200

    reformat: request.cc request.hh

--- src/libauracle/aur/request.cc
+++ src/libauracle/aur/request.cc
@@ -28,29 +28,21 @@ void QueryParamFormatter(std::string* out, const HttpRequest::QueryParam& kv) {
 
 }  // namespace
 
-void RpcRequest::AddArg(std::string_view key, std::string_view value) {
-  args_.emplace_back(key, value);
-}
+void RpcRequest::AddArg(std::string_view key, std::string_view value) { args_.emplace_back(key, value); }
 
 std::vector<std::string> RpcRequest::Build(std::string_view baseurl) const {
   const auto next_span = [&](std::string_view s) -> std::string_view {
     // No chopping needed.
-    if (s.size() <= approx_max_length_) {
-      return s;
-    }
+    if (s.size() <= approx_max_length_) { return s; }
 
     // Try to chop at the final ampersand before the cutoff length.
     auto n = s.substr(0, approx_max_length_).rfind('&');
-    if (n != std::string_view::npos) {
-      return s.substr(0, n);
-    }
+    if (n != std::string_view::npos) { return s.substr(0, n); }
 
     // We found a single arg which is Too Damn Long. Look for the ampersand just
     // after the length limit and use all of it.
     n = s.substr(approx_max_length_).find('&');
-    if (n != std::string_view::npos) {
-      return s.substr(0, n + approx_max_length_);
-    }
+    if (n != std::string_view::npos) { return s.substr(0, n + approx_max_length_); }
 
     // We're at the end of the querystring and have no place to chop.
     return s;
@@ -63,24 +55,19 @@ std::vector<std::string> RpcRequest::Build(std::string_view baseurl) const {
   while (!sv.empty()) {
     const auto span = next_span(sv);
 
-    requests.push_back(
-        absl::StrCat(baseurl, "/rpc?", base_querystring_, "&", span));
+    requests.push_back(absl::StrCat(baseurl, "/rpc?", base_querystring_, "&", span));
     sv.remove_prefix(std::min(sv.length(), span.length() + 1));
   }
 
   return requests;
 }
 
-RpcRequest::RpcRequest(const HttpRequest::QueryParams& base_params,
-                       size_type approx_max_length)
-    : base_querystring_(absl::StrJoin(base_params, "&", &QueryParamFormatter)),
-      approx_max_length_(approx_max_length) {}
+RpcRequest::RpcRequest(const HttpRequest::QueryParams& base_params, size_type approx_max_length)
+    : base_querystring_(absl::StrJoin(base_params, "&", &QueryParamFormatter)), approx_max_length_(approx_max_length) {}
 
 // static
-RawRequest RawRequest::ForSourceFile(const Package& package,
-                                     std::string_view filename) {
-  return RawRequest(absl::StrCat("/cgit/aur.git/plain/", filename,
-                                 "?h=", UrlEscape(package.pkgbase)));
+RawRequest RawRequest::ForSourceFile(const Package& package, std::string_view filename) {
+  return RawRequest(absl::StrCat("/cgit/aur.git/plain/", filename, "?h=", UrlEscape(package.pkgbase)));
 }
 
 std::string RpcRequest::to_string() const {
@@ -96,9 +83,7 @@ std::string RpcRequest::to_string() const {
   return ss.str();
 }
 
-std::vector<std::string> RawRequest::Build(std::string_view baseurl) const {
-  return {absl::StrCat(baseurl, urlpath_)};
-}
+std::vector<std::string> RawRequest::Build(std::string_view baseurl) const { return {absl::StrCat(baseurl, urlpath_)}; }
 
 std::vector<std::string> CloneRequest::Build(std::string_view baseurl) const {
   return {absl::StrCat(baseurl, "/", reponame_)};
--- src/libauracle/aur/request.hh
+++ src/libauracle/aur/request.hh
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 
+#include <fmt/core.h>
+
 #include <string>
 #include <utility>
 #include <vector>
-
-#include <fmt/core.h>
 
 #include "package.hh"
 
@@ -15,7 +15,7 @@ namespace aur {
 struct Request {
   virtual ~Request() = default;
   virtual std::vector<std::string> Build(std::string_view baseurl) const = 0;
-  virtual std::string to_string() const = 0; // for debugging
+  virtual std::string to_string() const = 0;  // for debugging
 };
 
 struct HttpRequest : Request {
@@ -27,10 +27,9 @@ struct HttpRequest : Request {
 class RawRequest : public HttpRequest {
  private:
   std::string urlpath_;
- public:
-  static RawRequest ForSourceFile(const Package& package,
-                                  std::string_view filename);
 
+ public:
+  static RawRequest ForSourceFile(const Package& package, std::string_view filename);
   explicit RawRequest(std::string urlpath) : urlpath_(std::move(urlpath)) {}
 
   RawRequest(const RawRequest&) = delete;
@@ -47,9 +46,9 @@ class RawRequest : public HttpRequest {
 class CloneRequest : public Request {
  private:
   std::string reponame_;
+
  public:
-  explicit CloneRequest(std::string reponame)
-      : reponame_(std::move(reponame)) {}
+  explicit CloneRequest(std::string reponame) : reponame_(std::move(reponame)) {}
 
   CloneRequest(const CloneRequest&) = delete;
   CloneRequest& operator=(const CloneRequest&) = delete;
@@ -65,17 +64,18 @@ class CloneRequest : public Request {
 
 // A base class describing a GET request to the RPC endpoint of the AUR.
 class RpcRequest : public HttpRequest {
- using size_type = std::string_view::size_type;
+  using size_type = std::string_view::size_type;
+
  private:
   std::string base_querystring_;
   size_type approx_max_length_;
   HttpRequest::QueryParams args_;
+
  public:
   // Upper limit on aur.archlinux.org seems to be somewhere around 8k.
   static constexpr size_type kMaxUriLength = 8000;
 
-  RpcRequest(const HttpRequest::QueryParams& base_params,
-             size_type approx_max_length = kMaxUriLength);
+  RpcRequest(const HttpRequest::QueryParams& base_params, size_type approx_max_length = kMaxUriLength);
 
   RpcRequest(const RpcRequest&) = delete;
   RpcRequest& operator=(const RpcRequest&) = delete;
@@ -92,9 +92,7 @@ class RpcRequest : public HttpRequest {
 class InfoRequest : public RpcRequest {
  public:
   explicit InfoRequest(const std::vector<std::string>& args) : InfoRequest() {
-    for (const auto& arg : args) {
-      AddArg(arg);
-    }
+    for (const auto& arg : args) { AddArg(arg); }
   }
 
   InfoRequest(const InfoRequest&) = delete;
@@ -122,27 +120,13 @@ class SearchRequest : public RpcRequest {
   };
 
   static SearchBy ParseSearchBy(std::string_view searchby) {
-    if (searchby == "name") {
-      return SearchBy::NAME;
-    }
-    if (searchby == "name-desc") {
-      return SearchBy::NAME_DESC;
-    }
-    if (searchby == "maintainer") {
-      return SearchBy::MAINTAINER;
-    }
-    if (searchby == "depends") {
-      return SearchBy::DEPENDS;
-    }
-    if (searchby == "makedepends") {
-      return SearchBy::MAKEDEPENDS;
-    }
-    if (searchby == "optdepends") {
-      return SearchBy::OPTDEPENDS;
-    }
-    if (searchby == "checkdepends") {
-      return SearchBy::CHECKDEPENDS;
-    }
+    if (searchby == "name") { return SearchBy::NAME; }
+    if (searchby == "name-desc") { return SearchBy::NAME_DESC; }
+    if (searchby == "maintainer") { return SearchBy::MAINTAINER; }
+    if (searchby == "depends") { return SearchBy::DEPENDS; }
+    if (searchby == "makedepends") { return SearchBy::MAKEDEPENDS; }
+    if (searchby == "optdepends") { return SearchBy::OPTDEPENDS; }
+    if (searchby == "checkdepends") { return SearchBy::CHECKDEPENDS; }
     return SearchBy::INVALID;
   }
 
@@ -161,22 +145,14 @@ class SearchRequest : public RpcRequest {
  private:
   std::string SearchByToString(SearchBy by) {
     switch (by) {
-      case SearchBy::NAME:
-        return "name";
-      case SearchBy::NAME_DESC:
-        return "name-desc";
-      case SearchBy::MAINTAINER:
-        return "maintainer";
-      case SearchBy::DEPENDS:
-        return "depends";
-      case SearchBy::MAKEDEPENDS:
-        return "makedepends";
-      case SearchBy::OPTDEPENDS:
-        return "optdepends";
-      case SearchBy::CHECKDEPENDS:
-        return "checkdepends";
-      default:
-        return "";
+      case SearchBy::NAME: return "name";
+      case SearchBy::NAME_DESC: return "name-desc";
+      case SearchBy::MAINTAINER: return "maintainer";
+      case SearchBy::DEPENDS: return "depends";
+      case SearchBy::MAKEDEPENDS: return "makedepends";
+      case SearchBy::OPTDEPENDS: return "optdepends";
+      case SearchBy::CHECKDEPENDS: return "checkdepends";
+      default: return "";
     }
   }
 };
