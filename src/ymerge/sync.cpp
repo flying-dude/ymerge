669d811c4888d7a897869972425209dae539b2fa
flying-dude <dude@flyspace.dev>
2023-05-24 11:18:05 +0200

    iterate over repos while syncing

--- src/ymerge/sync.cpp
+++ src/ymerge/sync.cpp
@@ -1,10 +1,10 @@
 #include <filesystem>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-#include "log.hpp"
 #include "config.hpp"
 #include "create_temporary_file.hpp"
+#include "log.hpp"
 
 using namespace std;
 using namespace std::filesystem;
@@ -19,53 +19,46 @@ void sync() {
     path pkg = repo.get_path() / "pkg";
     path db = pkg / (repo.name + ".db.tar");
 
-    if (!is_directory(pkg)) {
-      sudo("mkdir", "--parents", pkg);
-    }
+    if (!is_directory(pkg)) { sudo("mkdir", "--parents", pkg); }
 
-    if (!exists(db)) {
-      sudo("repo-add", db);
-    }
+    if (!exists(db)) { sudo("repo-add", db); }
   }
 
   sudo("pacman", "--sync", "--refresh");
 
-  /*for (auto& r : config::get_repos()) {
-    r.sync();
-  }*/
+  for (auto& repo_ : config::get_repos()) {
+    path git_dir = repo_.get_path() / "git";
+    path allowed_signers_file = repo_.get_path() / "allowed_signers";
 
-  config::repo curated_aur_repoa = config::curated_aur_repo;
-  path git_dir = curated_aur_repoa.get_path() / "git";
-  path allowed_signers_file = curated_aur_repoa.get_path() / "allowed_signers";
+    if (!exists(git_dir)) {
+      sudo("mkdir", "--parents", git_dir.c_str());
+      sudo("git", "clone", "--depth", "1", "--", repo_.url, git_dir);
+    } else {
+      // https://stackoverflow.com/questions/2180270/check-if-current-directory-is-a-git-repository
+      cmd_options opt_rev_parse;
+      opt_rev_parse.stdout_file = "/dev/null";
 
-  if (!exists(git_dir)) {
-    sudo("mkdir", "--parents", git_dir.c_str());
-    sudo("git", "clone", "--depth", "1", "--", curated_aur_repoa.url, git_dir);
-  } else {
-    // https://stackoverflow.com/questions/2180270/check-if-current-directory-is-a-git-repository
-    cmd_options opt_rev_parse;
-    opt_rev_parse.stdout_file = "/dev/null";
+      exec_opt_throw(fmt::format("pkg dir does exist but is not a git repo: \"{}\"", git_dir.c_str()), opt_rev_parse,
+                     "sudo", "git", "-C", git_dir.c_str(), "rev-parse", "--is-inside-work-tree");
 
-    exec_opt_throw(fmt::format("pkg dir does exist but is not a git repo: \"{}\"", git_dir.c_str()),
-                   opt_rev_parse, "sudo", "git", "-C", git_dir.c_str(), "rev-parse", "--is-inside-work-tree");
+      // https://stackoverflow.com/questions/41075972/how-to-update-a-git-shallow-clone
+      sudo("git", "-C", git_dir.c_str(), "fetch", "--depth", "1");
+      sudo("git", "-C", git_dir.c_str(), "reset", "--hard", "origin/main");
+    }
 
-            // https://stackoverflow.com/questions/41075972/how-to-update-a-git-shallow-clone
-    sudo("git", "-C", git_dir.c_str(), "fetch", "--depth", "1");
-    sudo("git", "-C", git_dir.c_str(), "reset", "--hard", "origin/main");
-  }
+    // verify the current git commit is signed by an authorized user
+    if (!exists(allowed_signers_file)) {
+      auto tmpfile = temporary_file::New();
 
-  // verify the current git commit is signed by an authorized user
-  if (!exists(allowed_signers_file)) {
-    auto tmpfile = temporary_file::New();
+      cout << "tmpfile: " << tmpfile << endl;
+      std::ofstream output(tmpfile->path);
+      output << repo_.allowed_signers;
+      output.close();
 
-    cout << "tmpfile: " << tmpfile << endl;
-    std::ofstream output(tmpfile->path);
-    output << curated_aur_repoa.allowed_signers;
-    output.close();
-
-    sudo("cp", tmpfile->path, allowed_signers_file);
-    sudo("git", "-C", git_dir.c_str(), "config", "gpg.ssh.allowedSignersFile", allowed_signers_file.c_str());
+      sudo("cp", tmpfile->path, allowed_signers_file);
+      sudo("git", "-C", git_dir.c_str(), "config", "gpg.ssh.allowedSignersFile", allowed_signers_file.c_str());
+    }
   }
 }
 
-} // namespace ymerge
+}  // namespace ymerge
