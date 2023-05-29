d70c90208dffa524e0a207aff945813be3902584
flying-dude <dude@flyspace.dev>
2023-05-29 16:34:25 +0200

    nspawn_opt(): print working dir, if it was specified

--- src/ymerge/nspawn.cpp
+++ src/ymerge/nspawn.cpp
@@ -18,13 +18,14 @@ void init_nspawn() {
 
   try {
     info("nspawn directory not present, creating it: {}", nspawn_dir.c_str());
-    filesystem::create_directories(makepkg_dir); // subfolder of nspawn_dir
+    filesystem::create_directories(nspawn_dir / "makepkg");  // create together with makepkg folder
     init_nspawn_throws();
   } catch (const std::exception &err) {
     try {
       // delete incomplete container setup
       std::filesystem::remove_all(nspawn_dir);
-    } catch (const std::exception &err2) { /* ignore */
+    } catch (const std::exception &err2) {
+      throw std::runtime_error(fmt::format("creating nspawn failed :: {} :: {}", err.what(), err2.what()));
     }
     throw std::runtime_error(fmt::format("creating nspawn failed: {}", err.what()));
   }
@@ -39,7 +40,7 @@ void init_nspawn_throws() {
     throw std::runtime_error(R"(Cannot initialize build container for systemd-nspawn. systemd is not running.
 TODO can you actually use nspawn w/o systemd running??)");
 
-  // set up the base system for the container using pacstrap
+  // use pacstrap to set up the base system for the container
   exec("pacstrap", "-c", nspawn_dir, "base", "base-devel", "sudo");
 
   // configure pacman
@@ -71,12 +72,18 @@ root ALL=(ALL) ALL
 %wheel ALL=(ALL) NOPASSWD: ALL
 )");
 
+  // creating a user, since makepkg will complain if we run it as root
   nspawn("useradd", "--create-home", "--groups", "wheel", "ymerge");
 
   /*
    * makepkg steps:
-   * 1) update system (but do not sync dbs, use host dbs)
-   * 2) makepkg --syncdeps
+   * 1) bind-mounts:
+   *   * bind-mount: /var/cache/pacman/pkg
+   *   * bind-mount: /var/lib/pacman/sync
+   *   * use atexit to unmount: https://cplusplus.com/reference/cstdlib/atexit/
+   *   * use ymerge lockfile to prevent remount (wrap entire makepkg step into locking step)
+   * 2) update the system (but do not sync dbs, use host dbs)
+   * 3) makepkg --syncdeps (without install, use nspawn_opt to change working dir)
    */
 
   todo("implement init_nspawn()");
--- src/ymerge/nspawn.hpp
+++ src/ymerge/nspawn.hpp
@@ -3,14 +3,19 @@
 namespace ymerge {
 
 extern std::filesystem::path nspawn_dir;
-extern std::filesystem::path makepkg_dir;
 
 extern void init_nspawn();
 
 template <typename... T>
 FMT_INLINE void nspawn_opt(fly::cmd_options opt, T &&...args) {
-  exec_print(opt, fmt::color::teal, "nspawn", args...);
-  cmd_opt(opt, "systemd-nspawn", "-D", nspawn_dir, args...);
+  if (!opt.working_dir) {
+    exec_print(opt, fmt::color::teal, "nspawn", args...);
+    cmd_opt(opt, "systemd-nspawn", "-D", nspawn_dir, args...);
+  } else {
+    std::string& working_dir = *opt.working_dir;
+    exec_print(opt, fmt::color::teal, "nspawn", working_dir, " :: ", args...);
+    cmd_opt(opt, "systemd-nspawn", "-D", nspawn_dir, fmt::format("--chdir={}", working_dir), args...);
+  }
 }
 
 template <typename... T>
